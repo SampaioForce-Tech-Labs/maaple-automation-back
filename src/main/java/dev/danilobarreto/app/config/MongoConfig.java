d0508911042a478001adc4e91807ed63fbc5b834
Danilo Lima <danilobarreto15@gmail.com>
2024-12-03 10:57:21 -0300

    Ajuste dos documentos Listar, e Upload. Observação: fazer os ajuste para o download.

--- src/main/java/dev/danilobarreto/app/config/MongoConfig.java
+++ src/main/java/dev/danilobarreto/app/config/MongoConfig.java
@@ -2,9 +2,11 @@ package dev.danilobarreto.app.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
 @Configuration
 public class MongoConfig {
@@ -14,6 +16,10 @@ public class MongoConfig {
         return MongoClients.create("mongodb://mongo:QIybfPwQZFVSkLdNNZGmKNNLhnTYqvVh@autorack.proxy.rlwy.net:13118/admin?authSource=admin");
     }
 
+    @Bean
+    public GridFsTemplate gridFsTemplate() throws Exception {
+        return new GridFsTemplate(mongoTemplate().getMongoDatabaseFactory(), mongoTemplate().getConverter());
+    }
     @Bean
     public MongoTemplate mongoTemplate() {
         return new MongoTemplate(mongoClient(), "automatizador");
--- src/main/java/dev/danilobarreto/app/controller/DocumentoController.java
+++ src/main/java/dev/danilobarreto/app/controller/DocumentoController.java
@@ -1,14 +1,25 @@
 package dev.danilobarreto.app.controller;
 
 
+import com.itextpdf.io.exceptions.IOException;
+import com.mongodb.client.gridfs.GridFSDownloadStream;
+import com.mongodb.client.gridfs.model.GridFSFile;
 import dev.danilobarreto.app.model.mongoDB.Documento;
 import dev.danilobarreto.app.service.DocumentoService;
+import org.apache.commons.compress.utils.IOUtils;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -16,53 +27,75 @@ import java.util.List;
 public class DocumentoController {
 
     @Autowired
+    private GridFsTemplate gridFsTemplate;
+
+    @Autowired
     private DocumentoService documentoService;
 
     @GetMapping("/listar")
-    public ResponseEntity<List<Documento>> listarDocumentos() {
-        List<Documento> documentos = documentoService.listarDocumentos();
-        return ResponseEntity.ok(documentos);
+    public ResponseEntity<List<Map<String, String>>> listarDocumentos() {
+        List<GridFSFile> documentos = documentoService.listarDocumentos();
+
+        List<Map<String, String>> documentosListados = documentos.stream()
+                .map(doc -> {
+                    Map<String, String> docInfo = new HashMap<>();
+                    docInfo.put("id", doc.getId().toString());
+                    docInfo.put("nome", doc.getFilename());
+                    return docInfo;
+                })
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(documentosListados);
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadDocumento(@RequestParam("arquivo") MultipartFile arquivo) {
-        if (arquivo.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Arquivo não pode estar vazio");
-        }
+    public String uploadDocumento(@RequestParam("file") MultipartFile file) {
         try {
-            documentoService.salvarDocumento(arquivo);
-            return ResponseEntity.status(HttpStatus.CREATED).body("Documento salvo com sucesso");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao salvar documento");
+            documentoService.salvarDocumento(file);
+            return "Documento enviado com sucesso!";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Erro ao salvar o documento: " + e.getMessage();
+        } catch (java.io.IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    @GetMapping("/download/{documentoId}")
-    public ResponseEntity<byte[]> downloadDocumento(@PathVariable String documentoId) {
+    @GetMapping("/download/{id}")
+    public ResponseEntity<byte[]> downloadDocumento(@PathVariable String id) {
         try {
-
-            Documento documento = documentoService.buscarDocumentoPorId(documentoId);
-
-            if (documento == null) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            if (id.length() != 24 || !ObjectId.isValid(id)) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("ID de documento inválido".getBytes());
             }
 
-            byte[] fileContent = documento.getConteudo();
+            // Convertendo o ID para ObjectId
+            ObjectId objectId = new ObjectId(id);
 
+            // Busca o arquivo no GridFS usando o ObjectId
+            GridFSFile file = documentoService.buscarDocumentoPorId(String.valueOf(objectId));
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_PDF);
-            headers.setContentDisposition(ContentDisposition.builder("attachment")
-                    .filename(documento.getNome())
-                    .build());
+            if (file != null) {
+                // Obtém o fluxo de download do GridFS
+                GridFSDownloadStream downloadStream = (GridFSDownloadStream) gridFsTemplate.getResource(file.getId().toString()).getInputStream();
 
+                // Converte o conteúdo do arquivo em bytes
+                byte[] fileContent = IOUtils.toByteArray(downloadStream);
 
-            return new ResponseEntity<>(fileContent, headers, HttpStatus.OK);
-
+                // Retorna o arquivo com os headers adequados
+                return ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                        .body(fileContent);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
         } catch (Exception e) {
-
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            // Logando erro e retornando Internal Server Error
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
 }
+
