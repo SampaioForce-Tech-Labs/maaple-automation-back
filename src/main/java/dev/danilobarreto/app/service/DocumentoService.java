f34d21bd1a8637aa0ff0a754afafc6eac376d36e
Danilo Lima <danilobarreto15@gmail.com>
2024-12-03 10:57:27 -0300

    Ajuste dos documentos Listar, e Upload. Observação: fazer os ajuste para o download.

--- src/main/java/dev/danilobarreto/app/service/DocumentoService.java
+++ src/main/java/dev/danilobarreto/app/service/DocumentoService.java
@@ -1,19 +1,22 @@
 package dev.danilobarreto.app.service;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import dev.danilobarreto.app.model.mongoDB.Documento;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.model.GridFSFile;
 import dev.danilobarreto.app.repository.DocumentoRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,33 +24,38 @@ public class DocumentoService {
 
     private static final Logger logger = LoggerFactory.getLogger(DocumentoService.class);
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Autowired
-    private DocumentoRepository documentoRepository;
 
     @Autowired
     private GridFsTemplate gridFsTemplate;
 
 
-    public void salvarDocumento(MultipartFile arquivo) {
-        try {
-            DBObject doc = new BasicDBObject();
-            doc.put("nome", arquivo.getOriginalFilename());
-            doc.put("arquivo", arquivo.getBytes());
+    public void salvarDocumento(MultipartFile arquivo) throws IOException {
+        InputStream inputStream = arquivo.getInputStream();
 
-            mongoTemplate.insert(doc, "documentos");
-        } catch (IOException e) {
-            e.printStackTrace();
+        gridFsTemplate.store(inputStream, arquivo.getOriginalFilename(), arquivo.getContentType());
+    }
+
+
+    public GridFSFile buscarDocumentoPorId(String id) {
+        try {
+            ObjectId objectId = new ObjectId(id);
+
+            GridFSFile file = gridFsTemplate.findOne(new org.springframework.data.mongodb.core.query.Query().addCriteria(org.springframework.data.mongodb.core.query.Criteria.where("_id").is(objectId)));
+
+            if (file != null) {
+                return file;
+            } else {
+                throw new RuntimeException("Documento não encontrado");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar o documento: " + e.getMessage());
         }
     }
-    public Documento buscarDocumentoPorId(String documentoId) {
-        return documentoRepository.findById(documentoId).orElse(null);
-    }
 
-
-    public List<Documento> listarDocumentos() {
-        return documentoRepository.findAll();
+    public List<GridFSFile> listarDocumentos() {
+        List<GridFSFile> documentos = new ArrayList<>();
+        gridFsTemplate.find(Query.query(Criteria.where("filename").exists(true)))
+                .forEach(documentos::add);
+        return documentos;
     }
 }
